cc6ad01959ea30d5bcc05545797d83291736b6f3
Jimil <jimilp7@gmail.com>
2019-11-25 23:13:03 -0500

    Initial commit

---
+++ app/src/main/java/com/example/android/getfun/MainActivity.java
@@ -0,0 +1,69 @@
+package com.example.android.getfun;
+
+import android.content.Intent;
+import android.speech.RecognizerIntent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+    private ArrayList<String> results;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    public void getSpeechInput(View view) {
+
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(intent, 10);
+        } else {
+            Toast.makeText(this, "Your Device Don't Support Speech Input", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case 10:
+                if (resultCode == RESULT_OK && data != null) {
+                    results = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    getAnswer(results);
+
+                }
+                break;
+        }
+    }
+
+    public void getAnswer(ArrayList<String> result) {
+        ArrayList<String> possibleQueries = new ArrayList<String>() {
+            {
+                add("restaurants");
+            }
+        };
+        String[] userSpeech = result.get(0).split(" ");
+        ArrayList<String> userQueries = new ArrayList<>(Arrays.asList(userSpeech));
+        userQueries.retainAll(possibleQueries);
+        String userInterest = "";
+        if(!userQueries.isEmpty()){
+            userInterest  = userQueries.get(0);
+        }
+        else userQueries.add("None");
+        System.out.println("user interest is " + userQueries);
+
+    }
+}
